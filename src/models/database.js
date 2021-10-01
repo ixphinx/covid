a5fb359ab176853a77df87195dfc711c87b8569d
ixphinx <48696004+ixphinx@users.noreply.github.com>
2021-10-01 16:12:57 -0300

    Update database

--- src/models/database.js
+++ src/models/database.js
@@ -3,85 +3,14 @@ const {Schema} = mongoose;
 
 const Data = new Schema({
     
-    /*----- Divisionales-------*/
-    divisional: String,
-    
-    //// Temporada ///////////////////////////////////
-    temporada: String,
-    
-    /*---- Cuentas de Usuarios ----*/
+    name: String,
+    lastname: String,
+    age: Number,
+    city: String,
+    country: String,
+    email: String,
     user: String,
-    password: String,
-    username: String,
-    userlastname: String,
-    aproved: Boolean,
-    usertype: String,
-    
-
-    /*---- Jugadores ----- */
-    jnombre: String,
-    jimg: String,
-    japellido: String,
-    jcedula: String,
-    jgoles: Number,
-    jtarjetasam: Number,
-    jtarjetasdam: Number,
-    jtarjestasroj: Number,
-    jamonestaciones: String,
-    jamonestacionid: String,
-    jfechaamonestacion: String,
-    jfechainicioamonestacion: String,
-    jequipo: String,
-
-    /*------ Equipos ----- */
-    enombre: String,
-    eescudo: String,
-    edivision: String,
-    
-
-    /*------- Torneos ------*/
-    tnombre: String,
-    tdivisional: String,
-
-
-    /*------- Fechas -------*/
-    fnombre: String,
-    ftorneo: String,
-
-
-    /*------- Partidos ------*/
-    pfechaid: String,
-    pdate: String,
-    ptime: String,
-    pequipoa: String,
-    pequipob: String,
-    peamarcador: Number,
-    pebmarcador: Number,
-    peagoles: String,
-    pebgoles: String,
-    pamarillasa: String,
-    pamarillasb: String,
-    projasa: String,
-    projasb: String,
-    pfpa: Number,
-    pfpb: Number,
-    parbitro: String,
-    pobservaciones: String,
-    pcancha: String,
-    
-    // Estadisticas ///////////////////////////////////////////////////////
-    epartidoid: String,
-    eequipo: String,
-    ejugador: String,
-    egoles: String,
-    eamarillas: String,
-    erojas: String,
-
-    
-    
-    /*------ imagenes ----- */
-    imgdiv: String,
-    imgurl: String,
+    password: String
 
 
 });
